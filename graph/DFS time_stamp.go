e19dd71f80ae9f2f3acad133e1e7f38a153f87c3
Youyu-eyes <2630921077@qq.com>
2026-05-22 15:27:00 +0800

    DFS 树上时间戳

---
+++ graph/DFS time_stamp.go
@@ -0,0 +1,26 @@
+package graph
+
+func timeStamp(n int, g [][]int) ([]int, []int) {
+	in := make([]int, n)
+	out := make([]int, n)
+	clock := 0
+	var dfs func(int, int)
+	dfs = func(x, fa int) {
+		clock++
+		in[x] = clock // 递
+		for _, y := range g[x] {
+			if y != fa {
+				dfs(y, x)
+			}
+		}
+		out[x] = clock // 归
+	}
+	dfs(0, -1)
+
+	return in, out
+}
+
+// 判断 x 是否为 y 的祖先
+func isAncestor (in, out []int, x, y int) bool {
+	return in[x] < in[y] && in[y] <= out[x]
+}
