af83434ed32e8897bce83b2738dccd729ea57712
Youyu-eyes <2630921077@qq.com>
2026-06-10 19:53:33 +0800

    二维前缀和

---
+++ Data_Structrue/prefix_2D.go
@@ -0,0 +1,21 @@
+package Data_Structrue
+
+type NumMatrix [][]int
+
+func Constructor(matrix [][]int) NumMatrix {
+    m, n := len(matrix), len(matrix[0])
+    sum := make([][]int, m+1)
+    sum[0] = make([]int, n+1)
+    for i, row := range matrix {
+        sum[i+1] = make([]int, n+1)
+        for j, x := range row {
+            sum[i+1][j+1] = sum[i+1][j] + sum[i][j+1] - sum[i][j] + x
+        }
+    }
+    return sum
+}
+
+// 返回左上角在 (r1, c1)，右下角在 (r2, c2) 的子矩阵元素和
+func (s NumMatrix) SumRegion(r1, c1, r2, c2 int) int {
+    return s[r2+1][c2+1] - s[r2+1][c1] - s[r1][c2+1] + s[r1][c1]
+}
