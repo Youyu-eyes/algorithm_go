327db27149e5c1d3ca5ec7b0e596362cbccc28c4
Youyu-eyes <2630921077@qq.com>
2026-06-10 21:04:06 +0800

    分治优化DP

---
+++ DP/Divide_and_Conquer_DP.go
@@ -0,0 +1,28 @@
+package dp
+
+func Divide_and_Conquer(n int) {
+	f := make([]int, n + 1)
+    nf := make([]int, n + 1)
+
+	w := func(j, i int) int {
+		return j + i  // **根据题目修改**
+	}
+
+	var op func(int, int, int, int)
+	op = func(l, r, optL, optR int) {
+		if l > r {
+			return
+		}
+
+		mid := l + (r - l) >> 1
+		best := -1
+		for j := optL; j <= min(optR, mid - 1); j++ {
+			if best < 0 || f[j] + w(j, mid) < f[best] + w(best, mid) {  // **根据题目修改**
+				best = j
+			}
+		}
+		nf[mid] = f[best] + w(best, mid)
+		op(l, mid - 1, optL, best)
+		op(mid + 1, r, best, optR)
+	}
+}
