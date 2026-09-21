c55487628dff7b4fe4b7de5ebdec9496af9bda23
Youyu-eyes <2630921077@qq.com>
2026-09-21 21:25:59 +0800

    dinic 算法

--- graph/dinic.go
+++ graph/dinic.go
@@ -39,10 +39,9 @@ func dinic(n int, edges [][]int, s, t int) (maxFlow int) {
 			return flow
 		}
 
-		for cur[u] < len(g[u]) {
+		for ; cur[u] < len(g[u]); cur[u]++ {
 			e := &g[u][cur[u]]
 			v, wt, rev := e.to, e.wt, e.rev
-			cur[u]++
 
 			if level[v] == level[u] + 1 && wt > 0 {
 				push := dfs(v, min(flow - pushed, wt))
